2e3edd02f52d68bd920160ea85da5d264e056b69
Andrej <andrej.bartulinvr@gmail.com>
2022-04-05 21:45:48 +0200

    Old client time update

--- old/old.py
+++ old/old.py
@@ -1,15 +1,42 @@
 import os
+from os.path import exists
 import sys
+import time
 import json
 import requests
 
 # Authentication for user filing issue
 USERNAME = 'VanGutan' # change this to own username
-PASSWORD = 'token' # change this to own token
+PASSWORD = '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' # change this to own token
 
 # The repository to add this issue to
 REPO_OWNER = 'ringwormGO-organization'
 repo_name = None
+
+def help():
+    print("<repo name> <title> <body>")
+    exit(0)
+
+def write_time():
+    if exists(".time.txt"):
+        os.remove(".time.txt")
+
+    f = open(".time.txt", "a+")
+    f.write(str(time.time()))
+
+def check_time() -> int:
+    if exists(".time.txt"):
+        write_time()
+        f = open(".time.txt", "r")
+        tmp_time = float(f.read())
+        if tmp_time + 600 == tmp_time:
+            return 0
+        else:
+            print("You need to wait 10 minutes from the last issue")
+            return 1
+    else:
+        print("Can't check time, writing from this moment!")
+        return 2
 
 def make_github_issue(title, body=None):
     #Create an issue on github.com using the given parameters.
@@ -26,14 +53,19 @@ def make_github_issue(title, body=None):
     r = session.post(url, json.dumps(issue))
     if r.status_code == 201:
         print(f"Successfully created issue {format(title)}")
+        write_time()
     else:
-        print(f"Could not create Issue {format(title)}")
+        print(f"Could not create issue {format(title)}")
         print("Response:", r.content)
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "-h":
-        print("<repo name> <title> <body>")
+    if not len(sys.argv) > 1:
+        help()
 
-    repo_name = sys.argv[1]
-    make_github_issue(sys.argv[2], sys.argv[3])
+    if sys.argv[1] == "-h":
+        help()
+
+    if check_time() != 1:
+        repo_name = sys.argv[1]
+        make_github_issue(sys.argv[2], sys.argv[3])
